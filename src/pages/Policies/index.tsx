b36d281f5eabf5925741efd1454a75adb48c188e
Pedro Rego <pedro.rego@stark.dk>
2024-03-02 22:36:48 +0000

    a

--- src/pages/Policies/index.tsx
+++ src/pages/Policies/index.tsx
@@ -1,39 +1,368 @@
 import { Box, Typography } from "@mui/material";
 import { i18n } from "../../translations/i18n";
 
-const Policies = () => {
+const RefundPolicies = () => {
   return (
     <Box
       style={{ display: "flex", justifyContent: "center", margin: "0px 20px" }}
     >
-      <Box style={{ marginTop: "100px", maxWidth: "500px" }}>
-        <Typography
-          style={{
-            fontSize: "35px",
-            fontWeight: 800,
-            textAlign: "center",
-            lineHeight: "49px",
-          }}
-        >
-          {i18n.t("pages.policies.termsOfService")}
-        </Typography>
-        <Typography
-          style={{
-            textAlign: "center",
-            marginTop: "20px",
-          }}
-        >
-          BA111OD may revise these Terms & conditions of use from time to time.
-          The Terms & Conditions in force at the time of purchase will apply to
-          the contract between you and BA111OD. You should therefore
-          occasionally visit this page to review the then current terms to which
-          you are bound. If you have any further questions do not hesitate to
-          contact us. We wish you an exciting experience with your new BA111OD
-          product.
-        </Typography>
+      <Box
+        style={{
+          marginTop: "100px",
+          maxWidth: "60%",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "40px",
+        }}
+      >
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "20px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text2"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text3"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text4"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title2")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text5"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text6"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text7"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text8"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text9"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text10"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title3")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text11"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title4")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text12"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text13"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title5")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text14"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text15"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text16"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title6")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text17"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title7")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text18"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title8")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text19"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title9")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text20"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title10")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text21"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title11")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text22"),
+            }}
+          />
+        </div>
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.termsOfService.title12")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.termsOfService.text23"),
+            }}
+          />
+        </div>
       </Box>
     </Box>
   );
 };
 
-export default Policies;
+export default RefundPolicies;
--- src/pages/RefundPolicies/index.tsx
+++ src/pages/RefundPolicies/index.tsx
@@ -6,31 +6,109 @@ const RefundPolicies = () => {
     <Box
       style={{ display: "flex", justifyContent: "center", margin: "0px 20px" }}
     >
-      <Box style={{ marginTop: "100px", maxWidth: "500px" }}>
-        <Typography
-          style={{
-            fontSize: "35px",
-            fontWeight: 800,
-            textAlign: "center",
-            lineHeight: "49px",
-          }}
-        >
-          {i18n.t("pages.refundPolicies.refundPolicies")}
-        </Typography>
-        <Typography
-          style={{
-            textAlign: "center",
-            marginTop: "20px",
-          }}
-        >
-          BA111OD may revise these Terms & conditions of use from time to time.
-          The Terms & Conditions in force at the time of purchase will apply to
-          the contract between you and BA111OD. You should therefore
-          occasionally visit this page to review the then current terms to which
-          you are bound. If you have any further questions do not hesitate to
-          contact us. We wish you an exciting experience with your new BA111OD
-          product.
-        </Typography>
+      <Box
+        style={{
+          marginTop: "100px",
+          maxWidth: "60%",
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "40px",
+        }}
+      >
+        <div>
+          <Typography
+            style={{
+              fontSize: "35px",
+              fontWeight: 800,
+              textAlign: "center",
+              lineHeight: "49px",
+            }}
+          >
+            {i18n.t("pages.refundPolicies.returnpolicy.title")}
+          </Typography>
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginTop: "20px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text2"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text3"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text4"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text5"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text6"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text7"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+              marginLeft: "30px",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text8"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text9"),
+            }}
+          />
+          <Typography
+            style={{
+              textAlign: "justify",
+            }}
+            dangerouslySetInnerHTML={{
+              __html: i18n.t("pages.refundPolicies.returnpolicy.text10"),
+            }}
+          />
+        </div>
       </Box>
     </Box>
   );

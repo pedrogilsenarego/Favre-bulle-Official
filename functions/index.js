511f4c45e0a7226985fb16c7f757445f380873f4
Pedro Rego <pedro.rego@stark.dk>
2024-03-21 15:47:54 +0000

    a

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const { sendEmail } = require("./sendEmail");
 const functions = require("firebase-functions");
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY_TEST);
@@ -84,6 +85,56 @@ app.post(
     response.send();
   }
 );
+
+app.post("/subscribe", async (req, res) => {
+  const email = req.body.email;
+  const getRequestParams = (email) => {
+    // get env variables
+    const API_KEY = process.env.MAILCHIMP_API_KEY;
+
+    const LIST_ID = process.env.MAILCHIMP_LIST_ID;
+    // mailchimp datacenter - mailchimp api keys always look like this:
+    // fe4f064432e4684878063s83121e4971-us6
+    // We need the us6 part
+    const DATACENTER = API_KEY.split("-")[1];
+
+    const url = `https://${DATACENTER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members`;
+    console.log(url);
+    // Add aditional params here. See full list of available params:
+    // https://mailchimp.com/developer/reference/lists/list-members/
+    const data = {
+      email_address: email,
+      status: "subscribed",
+    };
+
+    // Api key needs to be encoded in base 64 format
+    const base64ApiKey = Buffer.from(`anystring:${API_KEY}`).toString("base64");
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${base64ApiKey}`,
+    };
+
+    return {
+      url,
+      data,
+      headers,
+    };
+  };
+  try {
+    const { url, data, headers } = getRequestParams(email);
+
+    console.log(url);
+    const response = await axios.post(url, data, { headers });
+    // Success
+    return new Response(JSON.stringify("Thank you for the subscription"), {
+      status: 200,
+    });
+  } catch (error) {
+    if (error) {
+      console.log("error");
+    }
+  }
+});
 
 app.get("/test", (req, res) => {
   const userData = [{ teste: "teste" }, { teste: "teste" }];
